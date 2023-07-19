798b86c6ad1f9dedb6eb236fe8fd6f9f521f4e8b
JOY19ANYANGO <joyanyango402@gmail.com>
2023-07-19 17:51:37 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // Define a state variable for className
+  const [className, setClassName] = useState("");
+
+  // Function to update className when "Add to Cart" button is clicked
+  function changeClassName() {
+    // Update the className to "in-cart"
+    const updatedClassName = "in-cart";
+    setClassName(updatedClassName);
+  }
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={changeClassName}>
+        Add to Cart
+      </button>
     </li>
   );
 }
